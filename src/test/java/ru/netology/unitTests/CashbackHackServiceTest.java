9007bf16e305e7f7d22523f6e47e9e722402a837
Michael Urusov <toptun80@mail.ru>
2020-01-26 01:15:01 +0300

    added CashbackHackServiceTestData.csv & updated CashbackHackServiceTest

--- src/test/java/ru/netology/unitTests/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unitTests/CashbackHackServiceTest.java
@@ -1,6 +1,9 @@
 package ru.netology.unitTests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.service.CashbackHackService;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,34 +11,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
     private int amount;
     private int actual;
-    private int expected;
     private final CashbackHackService cashbackHackService = new CashbackHackService();
 
-    @Test
-    void shouldReturn1IfAmount999() {
-        amount = 999;
+    @DisplayName("Должен вернуть 1, если amount = 999")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackHackServiceTestData.csv", numLinesToSkip = 1)
+    void shouldReturn1IfAmount999(int amount, int expected, String message) {
         actual = cashbackHackService.remain(amount);
-        expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Должен вернуть 999, если amount = 1001")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackHackServiceTestData.csv", numLinesToSkip = 2)
+    void shouldReturn999IfAmount1001(int amount, int expected, String message) {
+        actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Должен вернуть 0, если значение amount кратно 1000")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackHackServiceTestData.csv", numLinesToSkip = 3)
+    void shouldReturnZeroIfAmountMultiple1000(int amount, int expected, String message) {
+        actual = cashbackHackService.remain(amount);
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldReturn999IfAmount1001() {
-        amount = 1001;
-        actual = cashbackHackService.remain(amount);
-        expected = 999;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldReturnZeroIfAmountMultiple1000() {
-        amount = 1000;
-        actual = cashbackHackService.remain(amount);
-        expected = 0;
-        assertEquals(expected, actual);
-    }
-
-    @Test
+    @DisplayName("Должен выбросить исключение, если amount = 0")
     public void shouldReturnIllegalArgumentExceptionIfAmountEqualsZero() throws IllegalArgumentException {
         amount = 0;
         assertThrows(IllegalArgumentException.class, () -> cashbackHackService.remain(amount));
